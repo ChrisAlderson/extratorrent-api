c2c722c81be2b60c64e36dcb0520b8edfd2d0efb
Chris Alderson <chrisalderson@protonmail.com>
2017-03-01 14:19:14 +0100

    added test for rss feed

--- test/extratorrent.js
+++ test/extratorrent.js
@@ -1,40 +1,63 @@
 'use strict';
 
-const chai = require('chai');
-const assert = chai.assert;
+const { assert } = require('chai');
 const ExtraTorrentAPI = require('../extratorrent-api');
 
 describe('ExtraTorrent', () => {
 
-  let extraTorrentAPI, query;
-  before(() => {
-    extraTorrentAPI = new ExtraTorrentAPI();
-    query = {
-      with_words: 'ettv',
-      without: '720p',
-      page: 2,
-      category: 'tv',
-      added: 7,
-      size_type: 'b'
-    };
-  });
+  describe('RSS', () => {
 
-  it('Should get results with a simple search', done => {
-    extraTorrentAPI.search('ettv')
-      .then(res => {
+    let rssAPI, query;
+    before(() => {
+      rssAPI = new ExtraTorrentAPI.RSS();
+
+      query = {
+        with_words: 'ettv',
+        category: 'tv'
+      };
+    });
+
+    it('should get results with an advanced search', done => {
+      rssAPI.search(query).then(res => {
         assert.isArray(res.results);
         assert.isNumber(res.total_results);
         done();
       }).catch(err => done(err));
+    });
+
   });
 
-  it('Should get results with an advanced search', done => {
-    extraTorrentAPI.search(query)
-      .then(res => {
+  describe('Website', () => {
+
+    let websiteAPI, query;
+    before(() => {
+      websiteAPI = new ExtraTorrentAPI.Website();
+      query = {
+        with_words: 'ettv',
+        without: '720p',
+        page: 2,
+        category: 'tv',
+        added: 7,
+        size_type: 'b'
+      };
+    });
+
+    it('should get results with a simple search', done => {
+      websiteAPI.search('ettv').then(res => {
         assert.isArray(res.results);
         assert.isNumber(res.total_results);
         done();
       }).catch(err => done(err));
+    });
+
+    it('should get results with an advanced search', done => {
+      websiteAPI.search(query).then(res => {
+        assert.isArray(res.results);
+        assert.isNumber(res.total_results);
+        done();
+      }).catch(err => done(err));
+    });
+
   });
 
 });
