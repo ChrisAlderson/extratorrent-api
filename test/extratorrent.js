74521013d952858c2e7906fee9265d49d555f216
ChrisAlderson <chrisalderson@protonmail.com>
2017-04-18 11:44:01 +0200

    added more tests for RRS and Website modules

--- test/extratorrent.js
+++ test/extratorrent.js
@@ -7,7 +7,7 @@ describe('ExtraTorrent', () => {
 
   describe('RSS', () => {
 
-    let rssAPI, query;
+    let rssAPI, query, queryNull;
     before(() => {
       rssAPI = new ExtraTorrentAPI.RSS();
 
@@ -15,12 +15,27 @@ describe('ExtraTorrent', () => {
         with_words: 'ettv',
         category: 'tv'
       };
+      queryNull = {
+        with_words: 'this should not return anything'
+      };
     });
 
     it('should get results with an advanced search', done => {
       rssAPI.search(query).then(res => {
         assert.isArray(res.results);
+        assert.isAtLeast(res.results.length, 1);
         assert.isNumber(res.total_results);
+
+        done();
+      }).catch(err => done(err));
+    });
+
+    it('should not get any results with an advanced search', done => {
+      rssAPI.search(queryNull).then(res => {
+        assert.isArray(res.results);
+        assert.strictEqual(res.results.length, 0);
+        assert.isNumber(res.total_results);
+
         done();
       }).catch(err => done(err));
     });
@@ -29,11 +44,19 @@ describe('ExtraTorrent', () => {
 
   describe('Website', () => {
 
-    let websiteAPI, query;
+    let websiteAPI, query, queryNull;
     before(() => {
       websiteAPI = new ExtraTorrentAPI.Website();
       query = {
         with_words: 'ettv',
+        without: '720p',
+        page: 2,
+        category: 'tv',
+        added: 7,
+        size_type: 'b'
+      };
+      queryNull = {
+        with_words: 'this should not return anything',
         without: '720p',
         page: 2,
         category: 'tv',
@@ -45,7 +68,19 @@ describe('ExtraTorrent', () => {
     it('should get results with a simple search', done => {
       websiteAPI.search('ettv').then(res => {
         assert.isArray(res.results);
+        assert.isAtLeast(res.results.length, 1);
         assert.isNumber(res.total_results);
+
+        done();
+      }).catch(err => done(err));
+    });
+
+    it('should not any get results with a simple search', done => {
+      websiteAPI.search('this should not return anything').then(res => {
+        assert.isArray(res.results);
+        assert.strictEqual(res.results.length, 0);
+        assert.isNumber(res.total_results);
+
         done();
       }).catch(err => done(err));
     });
@@ -53,7 +88,19 @@ describe('ExtraTorrent', () => {
     it('should get results with an advanced search', done => {
       websiteAPI.search(query).then(res => {
         assert.isArray(res.results);
+        assert.isAtLeast(res.results.length, 1);
         assert.isNumber(res.total_results);
+
+        done();
+      }).catch(err => done(err));
+    });
+
+    it('should not get any results with an advanced search', done => {
+      websiteAPI.search(queryNull).then(res => {
+        assert.isArray(res.results);
+        assert.strictEqual(res.results.length, 0);
+        assert.isNumber(res.total_results);
+
         done();
       }).catch(err => done(err));
     });
